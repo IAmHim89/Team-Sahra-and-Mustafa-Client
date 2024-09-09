d4287c9ac3c3073328b5c0ab35f4ac5b52b0ba1b
IAmHim89 <computertech0418@gmail.com>
2024-09-09 17:56:06 -0400

    working on home component

---
+++ src/Components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>The Canyon City Grill</h1>
+      <h2>Hours of Operation</h2>
+
+      <section></section>
+    </div>
+  );
+};
+
+export default Home;
