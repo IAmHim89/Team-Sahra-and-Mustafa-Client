bc35b3af6421ae710918929a6f46b0fb5734d83e
IAmHim89 <computertech0418@gmail.com>
2024-09-09 19:29:54 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 import Home from "./Components/Home";
+import About from "./Components/About";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
-      <nav></nav>
+      <nav>
+        <Header />
+      </nav>
       <header></header>
       <main>
         <Home />
+        <About />
       </main>
       <footer></footer>
     </div>
