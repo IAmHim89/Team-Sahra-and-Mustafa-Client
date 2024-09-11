c02133b6ec423845515c04107672e11de8a5f662
IAmHim89 <computertech0418@gmail.com>
2024-09-11 18:57:49 -0400

    about component created

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,25 +4,28 @@ import bbqImg from "../assets/images/BBQ.jpg";
 import burgerImg from "../assets/images/Bfries.jpg";
 import chickenImg from "../assets/images/ChickenWings.jpg";
 import cheeseSteakImg from "../assets/images/Phillycheese.jpg";
+import "../Components/Css_files/Home.css";
 
 const Home = () => {
   return (
-    <div className="carousel_container">
-      <Carousel
-        style={{
-          width: "75em",
-          height: "30em",
-          marginBottom: "2em",
-          marginTop: "2em",
-        }}
-        display="sequential"
-        className="rounded-box"
-      >
-        <Carousel.Item src={bbqImg} alt="City" />
-        <Carousel.Item src={burgerImg} alt="City" />
-        <Carousel.Item src={chickenImg} alt="City" />
-        <Carousel.Item src={cheeseSteakImg} alt="City" />
-      </Carousel>
+    <div>
+      <div className="carousel_container">
+        <Carousel
+          style={{
+            width: "100%",
+            height: "30em",
+            marginBottom: "2em",
+            marginTop: "2em",
+          }}
+          display="sequential"
+          className="rounded-box"
+        >
+          <Carousel.Item src={bbqImg} alt="City" />
+          <Carousel.Item src={burgerImg} alt="City" />
+          <Carousel.Item src={chickenImg} alt="City" />
+          <Carousel.Item src={cheeseSteakImg} alt="City" />
+        </Carousel>
+      </div>
       <div className="button_container">
         <Button color="accent">Order Online</Button>
       </div>
