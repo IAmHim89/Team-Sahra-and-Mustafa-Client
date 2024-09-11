28b5ba5dbcdad92fbdf49176b0af441d8436fd0d
IAmHim89 <computertech0418@gmail.com>
2024-09-10 20:19:24 -0400

    updated home and header component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,34 +1,30 @@
 import React from "react";
 import { Carousel, Button } from "react-daisyui";
+import bbqImg from "../assets/images/BBQ.jpg";
+import burgerImg from "../assets/images/Bfries.jpg";
+import chickenImg from "../assets/images/ChickenWings.jpg";
+import cheeseSteakImg from "../assets/images/Phillycheese.jpg";
+
 const Home = () => {
   return (
-    <div>
-      <div>
-        <section>
-          <Carousel className="rounded-box" display="sequential">
-            <Carousel.Item
-              src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-              alt="City"
-            />
-            <Carousel.Item
-              src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-              alt="City"
-            />
-            <Carousel.Item
-              src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-              alt="City"
-            />
-            <Carousel.Item
-              src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-              alt="City"
-            />
-          </Carousel>
-          <div>
-            <Button responsive={true} color="primary">
-              Menu
-            </Button>
-          </div>
-        </section>
+    <div className="carousel_container">
+      <Carousel
+        style={{
+          width: "75em",
+          height: "30em",
+          marginBottom: "2em",
+          marginTop: "2em",
+        }}
+        display="sequential"
+        className="rounded-box"
+      >
+        <Carousel.Item src={bbqImg} alt="City" />
+        <Carousel.Item src={burgerImg} alt="City" />
+        <Carousel.Item src={chickenImg} alt="City" />
+        <Carousel.Item src={cheeseSteakImg} alt="City" />
+      </Carousel>
+      <div className="button_container">
+        <Button color="accent">Order Online</Button>
       </div>
     </div>
   );
