b7d96d8655dda8f6cc0230eecf4a8c4e90b67645
IAmHim89 <computertech0418@gmail.com>
2024-09-11 21:13:31 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Header from "./Components/Header";
-<<<<<<< HEAD
 import Nav from "./Components/Nav";
-=======
 import Beverage from "./Components/Beverage";
->>>>>>> 44e46c651a2ff5be8ee66340ed441363f4fb3a21
 
 function App() {
   return (
     <div>
-      <nav></nav>
+      <nav>
+        <Nav />
+      </nav>
       <header>
         <Header />
       </header>
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,29 +1,32 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-
   return (
-      <nav className="navbar bg-base-100 shadow-md">
-        <div className="flex-1">
-          <Link to="/" className="btn btn-ghost normal-case text-xl">
+    <nav className="navbar bg-base-100 shadow-md">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
           Canyon City Grill
-          </Link>
-          <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/menu">Menu</Link></li>
-          <li><Link to="/reviews">Reviews</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
+        </Link>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/menu">Menu</Link>
+            </li>
+            <li>
+              <Link to="/reviews">Reviews</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-      
-
-
-  )
-  
- 
+  );
 };
 
 export default Nav;
