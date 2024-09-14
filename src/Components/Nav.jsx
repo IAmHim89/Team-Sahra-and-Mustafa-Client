a2360127b1a7f898f86b5d63a42d769fd17cc9f1
IAmHim89 <computertech0418@gmail.com>
2024-09-13 21:37:58 -0400

    updated nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Css_files/Nav.css";
 
 const Nav = () => {
   return (
-    <nav className="navbar bg-base-100 shadow-md">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
-          Canyon City Grill
-        </Link>
-      </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li>
-            <Link to="/reviews">Reviews</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="nav_container">
+      <nav className="navbar bg-base-100 shadow-md">
+        <div className="flex-1">
+          <Link
+            to="/"
+            className="btn btn-ghost normal-case text-xl canyon_city"
+          >
+            Canyon City Grill
+          </Link>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal p-0">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/reviews">Reviews</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button className="button_style">Menu</button>
+        </div>
+      </nav>
+    </div>
   );
 };
 
