3d4d674b3dd5a752ba6f6a57d5f95b9130fab524
IAmHim89 <computertech0418@gmail.com>
2024-09-16 17:20:44 -0400

    updated reviews

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -1,13 +1,9 @@
 import React from "react";
+import "./Css_files/Review.css";
 
 const Review = () => {
   return (
-    <div>
-      <script
-        src="https://static.elfsight.com/platform/platform.js"
-        data-use-service-core
-        defer
-      ></script>
+    <div className="review_container">
       <div
         className="elfsight-app-8267166a-5729-4fc6-8500-217ce99964cb"
         data-elfsight-app-lazy
