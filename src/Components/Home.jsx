76c9ef1b049f59c3e911cb4d01654dfe4710d874
sahraomar <sahraaliwrites@gmail.com>
2024-09-19 17:54:53 -0400

    added map

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,6 +34,20 @@ const Home = () => {
       <div className="spacing_line"></div>
       <About />
       <Beverage />
+      {/*imbed map here */}
+      <section className="my-10">
+        <h2 className="text-4xl font-bold text-center">Find Us Here</h2>
+        <div className="flex justify-center mt-6">
+          <iframe
+            className="w-full sm:w-4/5 md:w-3/5 lg:w-1/2 h-80"
+            style={{ border: 0 }}
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed?origin=mfe&pb=!1m4!2m1!1s14601+FM+306+Canyon+Lake,+TX+78133!5e0!6i12"
+          ></iframe>
+        </div>
+      </section>
     </div>
   );
 };
