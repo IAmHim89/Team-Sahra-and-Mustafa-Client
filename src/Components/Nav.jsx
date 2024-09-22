4ad7b525aacd205349afcd7592093f018100f9c1
sahraomar <sahraaliwrites@gmail.com>
2024-09-22 09:50:19 -0400

    updated contact/nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,37 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Css_files/Nav.css";
 import Contact from "./Contact";
-import ccgLogo from "../assets/images/CCG-Logo-100*76.jpg";
 
 const Nav = () => {
   return (
     <div className="nav_container">
-      <nav className="navbar bg-base-100 shadow-md">
-        <div className="flex-1">
-          <Link
-            to="/"
-            className="btn btn-ghost normal-case text-xl canyon_city"
-          >
-            {/* placeCCG logo here */}
-            <img src={logo} alt="Canyon City Grill" className="h-10 w-auto" />
-          </Link>
+      <nav className="navbar">
+        <div className="flex-1 justify-between items-center">
+          CCGLogo
+          <Link to="/">{/* placeCCG logo here */}</Link>
         </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal p-0">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/reviews">Reviews</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
+        {/*center the links here */}
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/reviews">Reviews</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        {/*menu button to the right*/}
         <div>
           <button className="button_style">Menu</button>
         </div>
