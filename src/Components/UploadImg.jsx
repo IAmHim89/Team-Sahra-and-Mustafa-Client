740fabc0e8addd89ef0b343b302d74f8822b5630
IAmHim89 <computertech0418@gmail.com>
2024-09-30 20:46:38 -0400

    updated image

--- src/Components/UploadImg.jsx
+++ src/Components/UploadImg.jsx
@@ -1,55 +1,84 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const UploadImg = () => {
   const [image, setImage] = useState("");
+  const [allImages, setAllImages] = useState([]);
+
+  useEffect(() => {
+    imgDownload();
+  }, []);
 
   function turnToBase64(e) {
-    console.log(e);
-    const reader = new FileReader();
-    reader.readAsDataURL(e.target.files[0]);
-    reader.onload = () => {
-      console.log(reader.result);
-      setImage(reader.result);
-    };
-
-    reader.onerror = (error) => {
-      console.log("Error", error);
-    };
+    setImage(e.target.files[0]);
   }
 
-  const imgUpload = async () => {
+  const imgUpload = async (e) => {
     try {
-      const response = await fetch("https://localhost:3001/image/uploadImg", {
+      console.log("test");
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("image", image);
+      formData.append("name", image);
+      const response = await fetch("http://localhost:3001/image/uploadImg", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("myToken"),
         },
-        body: JSON.stringify({
-          name: body.name,
-          myFile: image,
-        }),
+        body: formData,
       });
-      const json = await response.json()
+      const json = await response.json();
       console.log(json);
-      
     } catch (err) {
-      
-      };
+      console.log(err);
+    }
+  };
+
+  const imgDownload = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/image/all", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("myToken"),
+        },
+      });
+      const json = await response.json();
+      console.log(json);
+      setAllImages(json.allImages);
+    } catch (err) {
+      console.log(err);
     }
   };
 
   return (
     <div>
+      {allImages.map((image) => {
+        console.log(image);
+        const dataUrl = `data:${image.myFile.mimetype};base64,${image.myFile.buffer}`;
+        return (
+          <>
+            <img src={dataUrl} />
+          </>
+        );
+      })}
       <div>
         Lets Upload Image
-        <input accept="image/*" type="file" onChange={turnToBase64} />
-        {image == "" || image == null ? (
-          ""
-        ) : (
-          <img width={100} height={100} src={image} />
-        )}
-        <button onClick={imgUpload}>Upload Image</button>
+        <form onSubmit={imgUpload}>
+          <input name="name" type="text" placeholder="name" />
+          <input
+            name="image"
+            accept="image/*"
+            type="file"
+            onChange={turnToBase64}
+          />
+
+          {image == "" || image == null ? (
+            ""
+          ) : (
+            <img width={100} height={100} src={image} />
+          )}
+          <button>Upload Image</button>
+        </form>
       </div>
     </div>
   );
