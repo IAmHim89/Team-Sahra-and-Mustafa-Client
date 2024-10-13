68983b90740a670b2d090c0e3cfce3e2b94657e0
IAmHim89 <computertech0418@gmail.com>
2024-10-13 16:56:29 -0400

    updated links Css

--- src/Components/Review.jsx
+++ src/Components/Review.jsx
@@ -4,6 +4,7 @@ import "./Css_files/Review.css";
 const Review = () => {
   return (
     <div className="review_container">
+      <h1>Reviews</h1>
       <div
         className="elfsight-app-8267166a-5729-4fc6-8500-217ce99964cb"
         data-elfsight-app-lazy
