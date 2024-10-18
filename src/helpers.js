75fd9034f9376aeccb42e2d215a1d7a339920294
IAmHim89 <computertech0418@gmail.com>
2024-10-17 21:46:30 -0400

    updated

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,7 @@ if (window.location.hostname.includes("localhost")) {
   APIURL = "http://localhost:8080";
 } else if (
   window.location.hostname.includes(
-    "https://team-sahra-and-mustafa-client.vercel.app/"
+    "https://team-sahra-and-mustafa-client.vercel.app"
   )
 ) {
   APIURL = "https://team-sahra-and-mustafa-server.vercel.app";
